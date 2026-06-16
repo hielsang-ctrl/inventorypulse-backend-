b1217af8d77853ac5e6f26db322fee6f7eaaacab
kelvin <joer88544@gmail.com>
2026-06-16 11:49:23 +0300

    routers updated

---
+++ app/routers/__init__.py
@@ -0,0 +1,21 @@
+from routers import auth as auth
+from routers import users as users
+from routers import categories as categories
+from routers import suppliers as suppliers
+from routers import products as products
+from routers import stock as stock
+from routers import alerts as alerts
+from routers import reports as reports
+from routers import dashboard as dashboard
+
+__all__ = [
+    "auth",
+    "users",
+    "categories",
+    "suppliers",
+    "products",
+    "stock",
+    "alerts",
+    "reports",
+    "dashboard",
+]
